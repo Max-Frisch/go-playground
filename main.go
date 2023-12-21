0d1646a41e1fcab8d2c16574cc585953220737a3
Max-Frisch <max-frisch@gmx.net>
2023-12-21 20:38:22 +0700

    strconv package

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var num int = 42
-	var name string = "bob"
-	var isTrue bool = true
-	var riches float64 = 555.55
-	fmt.Printf("variable num: %d is of type %T\n", num, num)
-	fmt.Printf("variable name: %s is of type %T\n", name, name)
-	fmt.Printf("variable isTrue: %v is of type %T\n", isTrue, isTrue)
-	fmt.Printf("variable riches: %.2f is of type %T\n", riches, riches)
+	var s string = "2222"
+	var wurst bool = true
+	i, err := strconv.Atoi(s)
+	fmt.Printf("%d, %T\n", i, i)
+	fmt.Printf("%t, %T\n", wurst, wurst)
+	fmt.Printf("%v, %T\n", err, err)
 }
