7b2ac8e7e22923a8f77de43df27526c17f171971
Max-Frisch <max-frisch@gmx.net>
2023-12-21 21:12:08 +0700

    comparison

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
-	var s string = "2222"
-	var wurst bool = true
-	i, err := strconv.Atoi(s)
-	fmt.Printf("%d, %T\n", i, i)
-	fmt.Printf("%t, %T\n", wurst, wurst)
-	fmt.Printf("%v, %T\n", err, err)
+	var a string = "wurst"
+	var b int = 12
+	println(a == b)
 }
