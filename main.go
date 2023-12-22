f3d31ffb84509b9fa342c9bdb1fba657f8f8856e
Max-Frisch <max-frisch@gmx.net>
2023-12-22 21:43:00 +0700

    go practice

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fruit := "apple"
-
-	if fruit == "apple" {
-		fmt.Println("I love apples")
-	} else if fruit == "orange" {
-		fmt.Println("Oranges are not apples")
-	} else {
-		fmt.Println("no appetite")
+	grades := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(grades)
+	fmt.Println(len(grades))
+	fruits := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(fruits)
+	fmt.Println(len(fruits))
+	for i := 0; i < len(fruits); i++ {
+		fmt.Println(fruits[i])
 	}
-
-	fmt.Println("some other code...")
+	for index, value := range grades {
+		fmt.Println(index, "=>", value)
+	}
 }
